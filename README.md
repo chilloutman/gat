1655a427188cafc40ca3ed4986a826f99ae619f5
Lucas Neiva <lucasneiv4@gmail.com>
2018-03-03 14:11:07 +0100

    Add README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gradle Application Tomcat Demo
+
+This demo project uses the Gradle [Application Plugin](https://docs.gradle.org/current/userguide/application_plugin.html) to build an application that uses and embedded [Apache Tomcat](http://tomcat.apache.org) server.
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,10 @@
 import java.io.File;
-import java.io.IOException;
 
-import javax.servlet.Servlet;
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
-import org.apache.catalina.WebResourceRoot;
 import org.apache.catalina.startup.Tomcat;
-import org.apache.catalina.webresources.StandardRoot;
 
 public class Main {
 
